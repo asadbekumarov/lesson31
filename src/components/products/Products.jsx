d18099f0206bd7c99a9f3f74ab28912f258d79c7
asadbekumarov1 <your-email@example.com>
2025-01-12 18:41:20 +0500

    endd

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -20,21 +20,26 @@ const Products = () => {
     <div>
       <div className="max-w-[1300px] m-auto px-5">
         <div className="p-5">
-          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <ul className="flex flex-col gap-4">
             {data.map((product) => (
-              <li
+              <div
                 key={product.id}
-                className="bg-white border border-gray-200 rounded-lg shadow-md p-4 flex flex-col items-center transition-transform duration-300 hover:shadow-lg hover:scale-1"
+                className="bg-white border border-gray-200 rounded-lg shadow-md p-4 flex items-center transition-transform duration-300 hover:shadow-lg hover:scale-1"
               >
                 <img
                   src={product.images[0]}
                   alt={product.title}
                   className="h-32 w-auto mb-4 rounded-md"
                 />
-                <h3 className="text-lg font-semibold mb-2 transition-colors duration-300 cursor-pointer hover:text-blue-600">
-                  {product.title}
-                </h3>
-              </li>
+                <div className="flex flex-col">
+                  <h3 className="text-lg font-semibold mb-2 transition-colors duration-300 cursor-pointer hover:text-blue-600">
+                    {product.title}
+                  </h3>
+                  <h3 className="text-sm font-semibold mb-2 transition-colors duration-300 cursor-pointer hover:text-blue-600">
+                    {product.description}
+                  </h3>
+                </div>
+              </div>
             ))}
           </ul>
         </div>
